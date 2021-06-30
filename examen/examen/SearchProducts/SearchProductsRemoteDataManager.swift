939b0bf77df55aa2698c7ff4f40f11fff4aa93e1
Adrián Jacobo <addh2.0.1@gmail.com>
2021-06-29 20:19:05 -0500

    [ADD] VIPER structure
    [DELETE] Unnecessary files

---
+++ examen/examen/SearchProducts/SearchProductsRemoteDataManager.swift
@@ -0,0 +1,12 @@
+//
+//  SearchProductsRemoteDataManager.swift
+//  examen
+//
+//  Created by BE-006 on 29/06/21.
+//
+
+import Foundation
+
+class SearchProductsRemoteDataManager:SearchProductsRemoteDataManagerProtocol{
+    
+}
