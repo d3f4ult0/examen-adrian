7712580ed352b490f54f109016c1cc79f8ef4a10
Adrián Jacobo <addh2.0.1@gmail.com>
2021-06-29 19:52:11 -0500

    [ADD] Main structure of the project

---
+++ examen/examen/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  examen
+//
+//  Created by BE-006 on 29/06/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
